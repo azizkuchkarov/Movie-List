a86bc6ac7c6e3a7e670d6bb0b424d21ca2f56c54
azizkuchkarov <aziz_kuchkarov91@mail.ru>
2023-01-19 14:54:33 +0500

    React time to learn

---
+++ src/components/app-filter/app-filter.js
@@ -0,0 +1,12 @@
+import "./app-filter.css";
+const AppFilter = () => {
+  return (
+    <div className="btn-group">
+      <button className="btn btn-dark">Barcha Kinolar</button>
+      <button className="btn btn-outline-dark">Mashxur Kinolar</button>
+      <button className="btn btn-dark">Eng kup kurilgan Kinolar</button>
+    </div>
+  );
+};
+
+export default AppFilter;
